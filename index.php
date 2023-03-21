42cbe4ede708cc7c18e6680fdee3148589a8faf9
DaveBeaut <117293937+DaveBeaut@users.noreply.github.com>
2023-03-21 11:39:23 +0100

    premier commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+require dirname(__FILE__) . "/controleur/configRacine.php";
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+if (isset($_GET["action"])) {
+	$action = $_GET["action"];
+}
+
+//Ajoute un controleur secondaire ($fichier) en fonction du metier ($action) :
+$fichier = redirigeVers($action);
+
+require RACINE . "/controleur/" . $fichier;
